5821e2cab5d339d151ae54a716cee44b40dbae2a
Himank Gupta <himank226@gmail.com>
2020-08-19 02:10:33 +0530

    update image size style

--- src/composite-components/image.tsx
+++ src/composite-components/image.tsx
@@ -12,18 +12,21 @@ import { GetStyle } from "rn-themed-tailwind";
 import { Image as CustomImage } from '../basic-components/customImage';
 
 const IMAGE_STYLE = StyleSheet.create({
+    2: { ...GetStyle("w-2 h-2") },
+    3: { ...GetStyle("w-3 h-3") },
     4: { ...GetStyle("w-4 h-4") },
+    5: { ...GetStyle("w-5 h-5") },
     6: { ...GetStyle("w-6 h-6") },
+    7: { ...GetStyle("w-7 h-7") },
     8: { ...GetStyle("w-8 h-8") },
+    9: { ...GetStyle("w-9 h-9") },
     10: { ...GetStyle("w-10 h-10") },
-    12: { ...GetStyle("w-12 h-12") },
-    16: { ...GetStyle("w-16 h-16") },
-    20: { ...GetStyle("w-20 h-20") },
-    32: { ...GetStyle("w-32 h-32") },
-    40: { ...GetStyle("w-40 h-40") },
-    48: { ...GetStyle("w-48 h-48") },
+    11: { ...GetStyle("w-11 h-11") },
+    100: { ...GetStyle("w-full h-full") },
+
 
 });
+
 
 type ImageProps = {
     [key: string]: any,
