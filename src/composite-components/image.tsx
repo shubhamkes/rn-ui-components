324d6cdf5fe7f7a9e3cb66a2e94068e019b17782
Shubham Kesarwani <shubham.kesarwani@drivezy.com>
2020-08-19 13:12:07 +0530

    update image style

--- src/composite-components/image.tsx
+++ src/composite-components/image.tsx
@@ -11,21 +11,25 @@ import { StyleSheet } from "react-native";
 import { GetStyle } from "rn-themed-tailwind";
 import { Image as CustomImage } from '../basic-components/customImage';
 
-const IMAGE_STYLE = StyleSheet.create({
-    2: { ...GetStyle("w-2 h-2") },
-    3: { ...GetStyle("w-3 h-3") },
-    4: { ...GetStyle("w-4 h-4") },
-    5: { ...GetStyle("w-5 h-5") },
-    6: { ...GetStyle("w-6 h-6") },
-    7: { ...GetStyle("w-7 h-7") },
-    8: { ...GetStyle("w-8 h-8") },
-    9: { ...GetStyle("w-9 h-9") },
-    10: { ...GetStyle("w-10 h-10") },
-    11: { ...GetStyle("w-11 h-11") },
-    100: { ...GetStyle("w-full h-full") },
+// const IMAGE_STYLE = StyleSheet.create({
+//     2: { ...GetStyle("w-2 h-2") },
+//     3: { ...GetStyle("w-3 h-3") },
+//     4: { ...GetStyle("w-4 h-4") },
+//     5: { ...GetStyle("w-5 h-5") },
+//     6: { ...GetStyle("w-6 h-6") },
+//     7: { ...GetStyle("w-7 h-7") },
+//     8: { ...GetStyle("w-8 h-8") },
+//     9: { ...GetStyle("w-9 h-9") },
+//     10: { ...GetStyle("w-10 h-10") },
+//     11: { ...GetStyle("w-11 h-11") },
+//     100: { ...GetStyle("w-full h-full") },
 
 
-});
+// });
+
+const GetImageStyle = (size) => {
+    return StyleSheet.create(GetStyle(`w-${size} h-${size}`));
+}
 
 
 type ImageProps = {
@@ -36,9 +40,7 @@ type ImageProps = {
 };
 
 const Image = ({ style, source, size = 4, ...props }: ImageProps) => {
-
-    style = [IMAGE_STYLE[size], style] as any;
-
+    style = [GetImageStyle(size), style];
     return <CustomImage source={source} {...props} style={style} />;
 };
 
