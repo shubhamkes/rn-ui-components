7997a8064e7830a2e6bc8ae5ed6e1f5e26cad4bd
Umang Maurya <umangmaurya@outlook.com>
2020-12-15 17:18:36 +0530

    ready btn color

--- src/basic-components/progressButton.tsx
+++ src/basic-components/progressButton.tsx
@@ -10,92 +10,106 @@ import AwesomeButtonBlue from '@umangmaurya/react-native-really-awesome-button/s
 import { ThemeContext } from 'rn-themed-tailwind';
 
 export const ProgressButton = ({ ...props }) => {
-  const { type = 'primary', onPress, children, disabled = false, progress = true, width = null } = props;
+    const { type = 'primary', onPress, children, disabled = false, progress = true, width = null } = props;
 
-  const { getColor, getStyle } = useContext(ThemeContext);
+    const { getColor, getStyle } = useContext(ThemeContext);
 
-  /* To give 100% width; without sending width in string  */
-  let widthStyle = { width: null, stretch: false };
-  if (width) {
-    widthStyle.width = width
-  } else {
-    widthStyle.width = width;
-    widthStyle.stretch = true
-  }
+    /* To give 100% width; without sending width in string  */
+    let widthStyle = { width: null, stretch: false };
+    if (width) {
+        widthStyle.width = width
+    } else {
+        widthStyle.width = width;
+        widthStyle.stretch = true
+    }
 
-  // Default Props
-  const commonStyleProps = {
-    ...getStyle('rounded-lg'),
-    progress: progress,
-    raiseLevel: 0,
-    ...widthStyle,
-  };
+    // Default Props
+    const commonStyleProps = {
+        ...getStyle('rounded-lg'),
+        progress: progress,
+        raiseLevel: 0,
+        ...widthStyle,
+    };
 
-  const primaryEnabled = {
-    backgroundColor: getColor('primaryColor-500'),
-    backgroundActive: getColor('primaryColor-500'),
-    textColor: getColor('whiteColor')
-  };
+    const primaryEnabled = {
+        backgroundColor: getColor('primaryColor-500'),
+        backgroundActive: getColor('primaryColor-500'),
+        textColor: getColor('whiteColor')
+    };
 
-  const primaryDisabled = {
-    backgroundColor: getColor('disabledColor'),
-    backgroundActive: getColor('disabledColor'),
-    textColor: getColor('text-disabledColor')
-  };
+    const primaryDisabled = {
+        backgroundColor: getColor('disabledColor'),
+        backgroundActive: getColor('disabledColor'),
+        textColor: getColor('text-disabledColor')
+    };
 
-  const secondaryEnabled = {
-    backgroundColor: getColor('whiteColor'),
-    backgroundActive: getColor('whiteColor'),
-    textColor: getColor('primaryColor-500'),
-    boderWidth: 2,
-    borderColor: getColor('primaryColor-500')
-  };
+    const secondaryEnabled = {
+        backgroundColor: getColor('whiteColor'),
+        backgroundActive: getColor('whiteColor'),
+        textColor: getColor('primaryColor-500'),
+        boderWidth: 2,
+        borderColor: getColor('primaryColor-500')
+    };
 
-  const acceptEnabled = {
-    backgroundColor: getColor('positiveColor'),
-    backgroundActive: getColor('positiveColor'),
-    textColor: getColor('whiteColor')
-  }
+    const acceptEnabled = {
+        backgroundColor: getColor('positiveColor'),
+        backgroundActive: getColor('positiveColor'),
+        textColor: getColor('whiteColor')
+    }
 
-  const rejectEnabled = {
-    backgroundColor: getColor('whiteColor'),
-    backgroundActive: getColor('whiteColor'),
-    textColor: getColor('negativeColor'),
-    boderWidth: 2,
-    borderColor: getColor('negativeColor')
-  }
+    const rejectEnabled = {
+        backgroundColor: getColor('whiteColor'),
+        backgroundActive: getColor('whiteColor'),
+        textColor: getColor('negativeColor'),
+        boderWidth: 2,
+        borderColor: getColor('negativeColor')
+    }
 
-  let buttonTheme = {};
-  let btnType = '';
+    const readyEnabled = {
+        backgroundColor: getColor('warnColor'),
+        backgroundActive: getColor('warnColor'),
+        textColor: getColor('whiteColor')
+    }
 
-  switch (type) {
-    case 'primary':
-      buttonTheme = disabled ? primaryDisabled : primaryEnabled;
-      btnType = 'primary';
-      break;
-    case 'secondary':
-      buttonTheme = disabled ? primaryDisabled : secondaryEnabled;
-      btnType = 'secondary';
-      break;
-    case 'accept':
-      buttonTheme = disabled ? primaryDisabled : acceptEnabled;
-      btnType = 'primary';
-      break;
-    case 'reject':
-      buttonTheme = disabled ? primaryDisabled : rejectEnabled;
-      btnType = 'secondary';
-      break;
-  }
+    let buttonTheme = {};
+    let btnType = '';
 
-  return (
-    <AwesomeButtonBlue
-      {...commonStyleProps}
-      type={btnType}
-      disabled={disabled}
-      {...buttonTheme}
-      onPress={next => onPress(next)}
-    >
-      {children}
-    </AwesomeButtonBlue>
-  );
+    switch (type) {
+        case 'primary':
+            buttonTheme = disabled ? primaryDisabled : primaryEnabled;
+            btnType = 'primary';
+            break;
+        case 'secondary':
+            buttonTheme = disabled ? primaryDisabled : secondaryEnabled;
+            btnType = 'secondary';
+            break;
+        case 'accept':
+            buttonTheme = disabled ? primaryDisabled : acceptEnabled;
+            btnType = 'primary';
+            break;
+        case 'reject':
+            buttonTheme = disabled ? primaryDisabled : rejectEnabled;
+            btnType = 'secondary';
+            break;
+        case 'ready':
+            buttonTheme = disabled ? primaryDisabled : readyEnabled;
+            btnType = 'primary';
+            break;
+        case 'complete':
+            buttonTheme = disabled ? primaryDisabled : primaryEnabled;
+            btnType = 'primary';
+            break;
+    }
+
+    return (
+        <AwesomeButtonBlue
+            {...commonStyleProps}
+            type={btnType}
+            disabled={disabled}
+            {...buttonTheme}
+            onPress={next => onPress(next)}
+        >
+            {children}
+        </AwesomeButtonBlue>
+    );
 };
